652686f4f1274fc0a745b987fb6de030ceb405cc
LeviathanZC <leviathan.zercomp@gmail.com>
2019-05-31 22:39:37 +0300

    Created clientside and serverside proxy classes. Added proxy, like a variable into main class of mod.

--- src/main/java/com/company/cavesplorer/Cavesplorer.java
+++ src/main/java/com/company/cavesplorer/Cavesplorer.java
@@ -1,8 +1,10 @@
 package com.company.cavesplorer;
 
+import com.company.cavesplorer.proxy.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -14,22 +16,26 @@ public class Cavesplorer {
     public static final String NAME = "Cavesplorer";
     public static final String VERSION = "0.0.1";
 
-    private static Logger logger;
+    @SidedProxy(clientSide = "com.company.cavesplorer.proxy.ClientProxy", serverSide = "com.company.cavesplorer.proxy.CommonProxy")
+    public static CommonProxy proxy;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         System.out.println("\u001B[32m" + "[Starting Test Mod PRE-INITIALIZATION]" + "\u001B[0m");
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         System.out.println("\u001B[32m" + "[Starting Test Mod INITIALIZATION]" + "\u001B[0m");
+        proxy.init(event);
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         System.out.println("\u001B[32m" + "[Starting Test Mod POST-INITIALIZATION]" + "\u001B[0m");
+        proxy.postInit(event);
     }
 }
